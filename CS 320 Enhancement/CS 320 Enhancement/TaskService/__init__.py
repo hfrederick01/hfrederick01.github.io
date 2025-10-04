a6df29ea6be01d651655febdf13eaef1134cb203
hfrederick01 <hfrederick0326@gmail.com>
2025-10-04 19:34:34 -0400

    Delete CS 320 Enhancement/CS 320 Enhancement directory

--- CS 320 Enhancement/CS 320 Enhancement/TaskService/__init__.py
+++
@@ -1,4 +0,0 @@
-#Programmer: Hannah Frederick
-#Date:09/20/2025
-#Program Description: Utilize in-memory data structures to store, edit, and delete contact, task, and appointment information for a mobile application. Utilize Pytest unit testing to verify that all requirements are working as intended
-#This file serves as a way of identifying that this folder serves as a package, allowing the import of other methods in the folder
